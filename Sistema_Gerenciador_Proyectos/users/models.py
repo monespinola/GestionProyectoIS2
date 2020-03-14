c9e77ad51fd4d795f3b8c746976f635ba7ca7750
monespinola <61349803+monespinola@users.noreply.github.com>
2020-03-14 15:21:14 -0400

    Creacion model usuario

--- Sistema_Gerenciador_Proyectos/users/models.py
+++ Sistema_Gerenciador_Proyectos/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Usuario(models.Model):
+	nombre = models.CharField(max_length = 50, help_text = "Ingrese el nombre del usuario")
+	apellido = models.CharField(max_length = 50, help_text = "Ingrese el apellido del usuario")
+
+	def __str__ (self):
+		return '%s, %s' % (self.apellido, self.nombre)
+
+	def get_absolute (self):
+		return reverse ('usuario-detail', args = [str(self.id)])

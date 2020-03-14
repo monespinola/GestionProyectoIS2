22f300e57c4515f32e74eabe4a79d584eb4df9fa
monespinola <61349803+monespinola@users.noreply.github.com>
2020-03-14 16:11:29 -0400

    model proyecto

--- Sistema_Gerenciador_Proyectos/users/models.py
+++ Sistema_Gerenciador_Proyectos/users/models.py
@@ -9,5 +9,40 @@ class Usuario(models.Model):
 	def __str__ (self):
 		return '%s, %s' % (self.apellido, self.nombre)
 
-	def get_absolute (self):
-		return reverse ('usuario-detail', args = [str(self.id)])
+    def get_absolute_url(self):
+        return reverse('usuario-detail', args=[str(self.id)])
+
+class Proyecto(models.Model):
+    """
+    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    """
+    # Campos
+    nombre_proyecto = models.CharField(max_length = 50, help_text="Ingrese el nombre del proyecto")
+    descripcion= models.CharField(max_length = 200, help_text="Descripción del proyecto")
+
+    estados_proyecto = (
+    	('a','Activo'),
+    	('i','Inactivo'),
+    	('f','Finalizado'),
+    )
+
+    estado = models.CharField(
+        max_length = 1,
+        choices = estados_proyecto,
+        blank = True,
+        default = 'a',
+        help_text = "Estados del proyecto"
+    )
+
+    def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Proyecto
+        """
+        return reverse('proyecto-detail', args=[str(self.id)])
+
+    def __str__ (self):
+		return '%s, %s' % (self.nombre_proyecto, self.estados_proyecto)
+
+    # Metadata
+    class Meta: 
+        ordering = ["estado"]

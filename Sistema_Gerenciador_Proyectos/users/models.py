5d0b0a71ef202758fd2e38233a086a87b5de6c39
Luján <54210289+lujanmir25@users.noreply.github.com>
2020-03-14 19:25:01 -0500

    Creacion modelo
    Clase proyecto

--- Sistema_Gerenciador_Proyectos/users/models.py
+++ Sistema_Gerenciador_Proyectos/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+class Proyecto(models.Model):
+    """
+    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    """
+    # Campos
+    nombre_proyecto = models.CharField(max_length=50, help_text="Ingrese el nombre del proyecto")
+    descripción= models.CharField(max_length=90, help_text="Descripción del proyecto")
+    
+    estados_proyecto= ('a','Activo',
+    'i','Inactivo',
+    'f','Finalizado')
+    estado= models.CharField(
+        max_length=1,
+        choices= estados_proyecto,
+        blank= True,
+        default= 'a',
+        help_text= 'Estados del proyecto'
+    )
+     def get_absolute_url(self):
+        """Retorna el URL para acceder a una instancia del proyecto en particular."""
+        return reverse('Proyecto', args=[str(self.id)])
 
-# Create your models here.
+    def __str__(self):
+        """Formato de nombre y estado del proyecto."""
+        return '{0}, {1}'.format(self.nombre_proyecto, self.estado)
+    
+    # Metadata
+    class Meta: 
+        ordering = ["estado"]
+        
+
+
+
+
+>>>>>>> Stashed changes
